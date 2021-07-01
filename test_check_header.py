27de6add4c48ffca3e52c9904dd0738e1ca739a8
terrajanis <53621804+terrajanis@users.noreply.github.com>
2021-07-01 23:53:09 +0300

    homework 3 lesson 3

---
+++ test_check_header.py
@@ -0,0 +1,11 @@
+import requests
+
+
+class TestCheckHeader:
+    def test_check_header(self):
+        response = requests.get("https://playground.learnqa.ru/api/homework_header")
+        headers = response.headers
+        expected_header = "Some secret value"
+        assert "x-secret-homework-header" in headers, "Cookie HomeWork doesn't exist"
+        actual_header = headers["x-secret-homework-header"]
+        assert expected_header == actual_header, f"{actual_header} is not equal to {expected_header}"
