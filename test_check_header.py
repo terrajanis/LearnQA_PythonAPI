4f0994c909a47f8c9068744280cb8befd20f59f7
terrajanis <53621804+terrajanis@users.noreply.github.com>
2021-07-01 23:55:06 +0300

    fix

--- test_check_header.py
+++ test_check_header.py
@@ -6,6 +6,6 @@ class TestCheckHeader:
         response = requests.get("https://playground.learnqa.ru/api/homework_header")
         headers = response.headers
         expected_header = "Some secret value"
-        assert "x-secret-homework-header" in headers, "Cookie HomeWork doesn't exist"
+        assert "x-secret-homework-header" in headers, "Header x-secret-homework-header  doesn't exist"
         actual_header = headers["x-secret-homework-header"]
         assert expected_header == actual_header, f"{actual_header} is not equal to {expected_header}"
